6e58519e03125f871d84118afe884c9cb2050e6a
Peter Karlovsek <peter.karlovsek@gmail.com>
2024-12-29 00:51:00 +0100

    Fix bugs, 0.6 ms

--- main.cpp
+++ main.cpp
@@ -17,28 +17,39 @@ int main(int argc, char* argv[]) {
 
   auto result = false;
 
-  const int loop = 100;
+  const int loop = 1000;
 
   auto start = std::chrono::high_resolution_clock::now();
+  int results_count = 0;
 
   for (int i = 0; i < loop; i++) {
     counter = 0;
+
     result = solve(board, __builtin_popcountll(board));
+    if (result) {
+      results_count++;
+    }
   }
   auto end = std::chrono::high_resolution_clock::now();
 
   if (result) {
-    std::cout << "Solved!" << std::endl;
+    for (const auto& solved_board : solved_boards) {
+      if (__builtin_popcountll(solved_board)) {
+        print_board(solved_board);
+      }
+    }
+    std::cout << "Solved! " << results_count << std::endl;
   } else {
     std::cout << "Not solved!" << std::endl;
   }
 
-  const auto loop_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1.0 / loop;
+  const auto loop_time =
+      std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - start).count() / loop;
 
   std::cout << "Time: " << loop_time << "ms" << std::endl;
 
-  counter = 57394;
-  std::cout << counter << std::endl;
+  /*counter = 20279;*/
+  std::cout << "Counter:" << counter << std::endl;
   std::cout << "Counter/ms: " << counter / loop_time << std::endl;
 
   return 0;
--- src/FastPegSolitaire.hpp
+++ src/FastPegSolitaire.hpp
@@ -1,57 +1,14 @@
+#include <array>
 #include <bitset>
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
+#include <iterator>
 #include <ostream>
 #include <utility>
 
 #include <vector>
-namespace FastPegSolitaire {
-std::uint64_t get_english_board() {
-  // English board initial state (33-bit representation)
-  return 0b111111111111111101111111111111111;  // One empty spot in the center
-}
-
-std::uint64_t get_one_move() {
-  // English board initial state (33-bit representation)
-  return 0b110001000000000000000000000000000;
-}
-
-inline std::vector<char> board_to_string(std::uint64_t board) {
-  std::vector<char> result(33, '0');
-  for (int i = 0; i < 33; i++) {
-    if (board & (1UL << i)) {
-      result[i] = 'X';
-      /*std::cout << i << " ";*/
-    }
-  }
-
-  /*std::cout << std::endl;*/
-  /*for (const auto c : result) {*/
-  /*  std::cout << c;*/
-  /*}*/
-  /*std::cout << std::endl;*/
-  return result;
-}
-
-inline void print_board(std::uint64_t board) {
-  auto board_str = board_to_string(board);
-  std::size_t index = 0u;
-
-  for (int i = 0; i < 7; i++) {
-    for (int j = 0; j < 7; j++) {
-      if ((i < 2 || i > 4) && (j < 2 || j > 4)) {
-        std::cout << " ";
-      } else {
-        std::cout << board_str[index];
-        ++index;
-      }
-    }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
-}
-
+namespace {
 std::uint64_t p33 = 1ul << 32;
 std::uint64_t p32 = 1ul << 31;
 std::uint64_t p31 = 1ul << 30;
@@ -86,34 +43,78 @@ std::uint64_t p3 = 1ul << 2;
 std::uint64_t p2 = 1ul << 1;
 std::uint64_t p1 = 1ul << 0;
 
-std::vector<std::pair<std::uint64_t, std::uint64_t>> can_moves{
+}  // namespace
+
+namespace FastPegSolitaire {
+std::uint64_t get_english_board() {
+  // English board initial state (33-bit representation)
+  return 0b111111111111111101111111111111111;  // One empty spot in the center
+}
+
+std::uint64_t get_one_move() {
+  return 0b110001000000000000000000000000000;
+}
+
+inline std::vector<char> board_to_string(std::uint64_t board) {
+  std::vector<char> result(33, '0');
+  for (int i = 0; i < 33; i++) {
+    if (board & (1UL << i)) {
+      result[i] = 'X';
+    }
+  }
+
+  return result;
+}
+
+inline void print_board(std::uint64_t board) {
+  auto board_str = board_to_string(board);
+  std::size_t index = 0u;
+
+  for (int i = 0; i < 7; i++) {
+    for (int j = 0; j < 7; j++) {
+      if ((i < 2 || i > 4) && (j < 2 || j > 4)) {
+        std::cout << " ";
+      } else {
+        std::cout << board_str[index];
+        ++index;
+      }
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+static const std::vector<std::pair<std::uint64_t, std::uint64_t>> can_moves{
     // Peg 33
-    {p33 | p32, p33 | p32 | p31},  // 33 right
     {p33 | p30, p33 | p30 | p25},  // 33 down
+    {p33 | p32, p33 | p32 | p31},  // 33 right
     // Peg 32
     {p32 | p29, p32 | p29 | p24},  // 32 down
     // Peg 31
     {p31 | p32, p31 | p32 | p33},  // 31 left
     {p31 | p28, p31 | p28 | p23},  // 31 down
     // Peg 30
-    {p30 | p29, p30 | p29 | p28},  // 30 right
     {p30 | p25, p30 | p25 | p18},  // 30 down
+    {p30 | p29, p30 | p29 | p28},  // 30 right
     // Peg 29
     {p29 | p24, p29 | p24 | p17},  // 29 down
     // Peg 28
     {p28 | p29, p28 | p29 | p30},  // 28 left
+    {p28 | p23, p28 | p23 | p16},  // 28 down
     // Peg 27
-    {p27 | p26, p27 | p26 | p25},  // 27 right
     {p27 | p20, p27 | p20 | p13},  // 27 down
-    {p26 | p25, p26 | p25 | p24},  // 26 right
+    {p27 | p26, p27 | p26 | p25},  // 27 right
+    // Peg 26
     {p26 | p19, p26 | p19 | p12},  // 26 down
+    {p26 | p25, p26 | p25 | p24},  // 26 right
+    // Peg 25
     {p25 | p26, p25 | p26 | p27},  // 25 left
     {p25 | p18, p25 | p18 | p11},  // 25 down
-    {p25 | p24, p25 | p24 | p23},  // 25 right
     {p25 | p30, p25 | p30 | p33},  // 25 up
+    {p25 | p24, p25 | p24 | p23},  // 25 right
     // Peg 24
     {p24 | p25, p24 | p25 | p26},  // 24 left
-    {p24 | p17, p14 | p17 | p10},  // 24 down
+    {p24 | p17, p24 | p17 | p10},  // 24 down
     {p24 | p29, p24 | p29 | p32},  // 24 up
     {p24 | p23, p24 | p23 | p22},  // 24 right
     // Peg 23
@@ -132,88 +133,85 @@ std::vector<std::pair<std::uint64_t, std::uint64_t>> can_moves{
     // Peg 19
     {p19 | p18, p19 | p18 | p17},  // 19 right
     // Peg 18
-    {p18 | p25, p18 | p25 | p30},  // 18 up
     {p18 | p19, p18 | p19 | p20},  // 18 left
     {p18 | p11, p18 | p11 | p6},   // 18 down
+    {p18 | p25, p18 | p25 | p30},  // 18 up
     {p18 | p17, p18 | p17 | p16},  // 18 right
     // Peg 17
-    {p17 | p24, p17 | p24 | p29},  // 17 up
-    {p17 | p10, p17 | p10 | p5},   // 17 down
-    {p17 | p16, p17 | p16 | p15},  // 17 right
     {p17 | p18, p17 | p18 | p19},  // 17 left
+    {p17 | p10, p17 | p10 | p5},   // 17 down
+    {p17 | p24, p17 | p24 | p29},  // 17 up
+    {p17 | p16, p17 | p16 | p15},  // 17 right
     // Peg 16
-    {p16 | p23, p16 | p23 | p28},  // 16 up
-    {p16 | p9, p16 | p9 | p4},     // 16 down
-    {p16 | p15, p16 | p15 | p14},  // 16 right
     {p16 | p17, p16 | p17 | p18},  // 16 left
+    {p16 | p9, p16 | p9 | p4},     // 16 down
+    {p16 | p23, p16 | p23 | p28},  // 16 up
+    {p16 | p15, p16 | p15 | p14},  // 16 right
     // Peg 15
     {p15 | p16, p15 | p16 | p17},  // 15 left
     // Peg 14
     {p14 | p15, p14 | p15 | p16},  // 14 left
     // Peg 13
-    {p13 | p12, p13 | p12 | p11},  // 13 right
     {p13 | p20, p13 | p20 | p27},  // 13 up
+    {p13 | p12, p13 | p12 | p11},  // 13 right
     // Peg 12
-    {p12 | p11, p12 | p11 | p10},  // 12 right
     {p12 | p19, p12 | p19 | p26},  // 12 up
-    {p12 | p13, p12 | p13 | p14},  // 12 left
+    {p12 | p11, p12 | p11 | p10},  // 12 right
     // Peg 11
-    {p11 | p18, p11 | p18 | p25},  // 11 up
-    {p11 | p6, p11 | p6 | p3},     // 11 down
-    {p11 | p10, p11 | p10 | p9},   // 11 right
     {p11 | p12, p11 | p12 | p13},  // 11 left
+    {p11 | p6, p11 | p6 | p3},     // 11 down
+    {p11 | p18, p11 | p18 | p25},  // 11 up
+    {p11 | p10, p11 | p10 | p9},   // 11 right
     // Peg 10
-    {p10 | p17, p10 | p17 | p24},  // 10 up
-    {p10 | p5, p10 | p5 | p2},     // 10 down
-    {p10 | p9, p10 | p9 | p8},     // 10 right
     {p10 | p11, p10 | p11 | p12},  // 10 left
+    {p10 | p5, p10 | p5 | p2},     // 10 down
+    {p10 | p17, p10 | p17 | p24},  // 10 up
+    {p10 | p9, p10 | p9 | p8},     // 10 right
     // Peg 9
-    {p9 | p16, p9 | p16 | p23},  // 9 up
-    {p9 | p4, p9 | p4 | p1},     // 9 down
-    {p9 | p8, p9 | p8 | p7},     // 9 right
     {p9 | p10, p9 | p10 | p11},  // 9 left
+    {p9 | p4, p9 | p4 | p1},     // 9 down
+    {p9 | p16, p9 | p16 | p23},  // 9 up
+    {p9 | p8, p9 | p8 | p7},     // 9 right
     // Peg 8
     {p8 | p9, p8 | p9 | p10},    // 8 left
     {p8 | p15, p8 | p15 | p22},  // 8 up
     // Peg 7
-    {p7 | p14, p7 | p14 | p21},  // 7 up
     {p7 | p8, p7 | p8 | p9},     // 7 left
+    {p7 | p14, p7 | p14 | p21},  // 7 up
     // Peg 6
     {p6 | p11, p6 | p11 | p18},  // 6 up
     {p6 | p5, p6 | p5 | p4},     // 6 right
     // Peg 5
     {p5 | p10, p5 | p10 | p17},  // 5 up
                                  // Peg 4
+    {p4 | p5, p4 | p5 | p6},     // 4 left
     {p4 | p9, p4 | p9 | p16},    // 4 up
-    {p4 | p5, p4 | p4 | p5},     // 4 left
     // Peg 3
     {p3 | p6, p3 | p6 | p11},  // 3 up
     {p3 | p2, p3 | p2 | p1},   // 3 right
     // Peg 2
     {p2 | p5, p2 | p5 | p10},  // 2 up
     // Peg 1
-    {p1 | p4, p1 | p4 | p9},  // 1 up
     {p1 | p2, p1 | p2 | p3},  // 1 left
+    {p1 | p4, p1 | p4 | p9},  // 1 up
 };
 
 std::uint64_t counter{};
 
-bool solve(std::uint64_t board, std::size_t pegs) {
+std::array<std::uint64_t, 33> solved_boards;
+
+bool solve(const std::uint64_t board, const std::size_t pegs) {
   ++counter;
 
   if (pegs == 1) {
-    std::cout << "counter: " << counter << "pegs: " << pegs << std::endl;
-    print_board(board);
+    solved_boards[0] = board;
     return true;
   }
 
-  /*else {*/
-  /*  std::cout << "number of pegs: " << __builtin_popcountll(board) << std::endl;*/
-  /*}*/
-
   for (const auto& can_move : can_moves) {
-    if (((board ^ can_move.first) & can_move.second) == 0 && solve(board ^ can_move.second, pegs - 1)) {
-      print_board(board);
+    const auto new_pegs = pegs - 1;
+    if (((board ^ can_move.first) & can_move.second) == 0 && solve(board ^ can_move.second, new_pegs)) {
+      solved_boards[new_pegs] = board;
       return true;
     }
   }
